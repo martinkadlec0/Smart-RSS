8ecf64efe7c5bde9855d70d13ef9706760ced66e
Martin Kadlec <harou2@gmail.com>
2013-09-13 14:35:51 +0200

    preven screen=undefined error

--- scripts/items.js
+++ scripts/items.js
@@ -63,6 +63,8 @@ window.addEventListener('blur', function() {
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
 
 function isScrolledIntoView(elem) {
+	if (!screen) return false;
+	
 	var docViewTop = 0;
 	var docViewBottom = screen.height;
 
