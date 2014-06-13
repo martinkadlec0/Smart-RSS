6be9f259e6e06b4a03779c95a5c80ba84a9e8f7a
Martin Kadlec <harou2@gmail.com>
2014-06-13 13:08:46 +0200

    set Cache-Control to no-store, no-cache, must-revalidate, post-check=0, pre-check=0

--- scripts/bgprocess/models/Loader.js
+++ scripts/bgprocess/models/Loader.js
@@ -211,7 +211,7 @@ define(['backbone', 'modules/RSSParser', 'modules/Animation'], function (BB, RSS
 				downloadURL();
 			},
 			beforeSend: function(xhr) {
-				xhr.setRequestHeader('Cache-Control', 'no-cache');
+				xhr.setRequestHeader('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 				xhr.setRequestHeader('Pragma', 'no-cache');
 				/* Removed because "http://www.f5haber.com/rss/teknoloji_haber.xml" doesn't like it
 				   xhr.setRequestHeader('If-Modified-Since', 'Tue, 1 Jan 1991 00:00:00 GMT');
