50645fb50baf232b22fae77d11f23d8af415bba8
zakius <jaroslaw.jarosik@outlook.com>
2019-02-15 23:51:29 +0100

    update

--- scripts/app/helpers/formatDate.js
+++ scripts/app/helpers/formatDate.js
@@ -7,36 +7,37 @@
  * @example formatDate(new Date, 'YYYY-MM-DD hh:mm');
  */
 define([], function() {
-	var formatDate = (function() {
-		var that;
-		var addZero = function(num) {
+	return formatDate = (function() {
+		let that;
+		const zeroPad = function(num) {
 			if (num < 10) num = '0' + num;
 			return num;
 		};
-		var na = function(n, z) {
+		const na = function(n, z) {
 			return n % z;
 		};
-		var getDOY = function() {
-			var dt = new Date(that);
+
+		const getDOY = function() {
+			const dt = new Date(that);
+			const firstJanuary = new Date(dt.getFullYear(), 0, 1);
 			dt.setHours(0, 0, 0);
-			var onejan = new Date(dt.getFullYear(), 0, 1);
-			return Math.ceil((dt - onejan) / 86400000);
+			return Math.ceil((dt - firstJanuary) / 86400000);
 		};
-		var getWOY = function() {
-			var dt = new Date(that);
+		const getWOY = function() {
+			const dt = new Date(that);
+			const firstJanuary = new Date(dt.getFullYear(), 0, 1);
 			dt.setHours(0, 0, 0);
 			dt.setDate(dt.getDate() + 4 - (dt.getDay() || 7));
-			var onejan = new Date(dt.getFullYear(), 0, 1);
-			return Math.ceil((((dt - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+			return Math.ceil((((dt - firstJanuary) / 86400000) + firstJanuary.getDay() + 1) / 7);
 		};
-		var dateVal = function(all, found) {
+		const dateVal = function(all, found) {
 			switch (found) {
 				case 'DD':
-					return addZero(that.getDate());
+					return zeroPad(that.getDate());
 				case 'D':
 					return that.getDate();
 				case 'MM':
-					return addZero(that.getMonth() + 1);
+					return zeroPad(that.getMonth() + 1);
 				case 'M':
 					return that.getMonth() + 1;
 				case 'YYYY':
@@ -44,19 +45,19 @@ define([], function() {
 				case 'YY':
 					return that.getFullYear().toString().substr(2, 2);
 				case 'hh':
-					return addZero(that.getHours());
+					return zeroPad(that.getHours());
 				case 'h':
 					return that.getHours();
 				case 'HH':
-					return addZero(na(that.getHours(), 12));
+					return zeroPad(na(that.getHours(), 12));
 				case 'H':
 					return na(that.getHours(), 12);
 				case 'mm':
-					return addZero(that.getMinutes());
+					return zeroPad(that.getMinutes());
 				case 'm':
 					return that.getMinutes();
 				case 'ss':
-					return addZero(that.getSeconds());
+					return zeroPad(that.getSeconds());
 				case 's':
 					return that.getSeconds();
 				case 'u':
@@ -86,6 +87,4 @@ define([], function() {
 			return str;
 		};
 	}());
-
-	return formatDate;
 });
