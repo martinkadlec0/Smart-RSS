a2f9ecc75db3b94d9dfd43c82768bfb1ab933e06
zakius <jaroslaw.jarosik@outlook.com>
2019-02-17 17:39:51 +0100

    readability

--- scripts/options/worker.js
+++ scripts/options/worker.js
@@ -9,70 +9,70 @@ var db;
 var content;
 
 
-request.addEventListener('error', function(e) {
-	throw 'Error code: ' + this.errorCode;
+request.addEventListener('error', function (e) {
+    throw 'Error code: ' + this.errorCode;
 });
 
-request.addEventListener('success', function(e) {
-	db = this.result;
-	if (content) startImport();
+request.addEventListener('success', function (e) {
+    db = this.result;
+    if (content) startImport();
 });
 
-onmessage = function(e) {
-	if (e.data.action == 'file-content') {
-		content = e.data.value;
-		if (db) startImport();
-	}
-}
+onmessage = function (e) {
+    if (e.data.action === 'file-content') {
+        content = e.data.value;
+        if (db) startImport();
+    }
+};
 
 var writes = 0;
 
 function handleReq(req) {
-	writes++;
-	req.onsuccess = req.onerror = function() {
-		writes--;
-		if (writes <= 0) {
-			postMessage({ action: 'finished' });
-		}
-	}
+    writes++;
+    req.onsuccess = req.onerror = function () {
+        writes--;
+        if (writes <= 0) {
+            postMessage({action: 'finished'});
+        }
+    };
 }
 
 function startImport() {
-	var tx = this.db.transaction(['folders-backbone', 'sources-backbone', 'items-backbone'], 'readwrite');
-	var folders = tx.objectStore('folders-backbone');
-	var sources = tx.objectStore('sources-backbone');
-	var items = tx.objectStore('items-backbone');
+    var transaction = this.db.transaction(['folders-backbone', 'sources-backbone', 'items-backbone'], 'readwrite');
+    var folders = transaction.objectStore('folders-backbone');
+    var sources = transaction.objectStore('sources-backbone');
+    var items = transaction.objectStore('items-backbone');
 
-	var f = content.folders;
-	var s = content.sources;
-	var t = content.items;
+    var importedFolders = content.folders;
+    var importedSources = content.sources;
+    var importedItems = content.items;
 
 
-	if (f) {
-		for (var i=0,j=f.length; i<j; i++) {
-			handleReq( folders.add(f[i]) );
-			if (!(i % 10)) postMessage({ action: 'message', value: 'Folders: ' + i + '/' + j });
-		}
-	}
+    if (importedFolders) {
+        for (let i = 0, j = importedFolders.length; i < j; i++) {
+            handleReq(folders.add(importedFolders[i]));
+            if (!(i % 10)) postMessage({action: 'message', value: 'Folders: ' + i + '/' + j});
+        }
+    }
 
-	if (s) {
-		for (var i=0,j=s.length; i<j; i++) {
-			handleReq( sources.add(s[i]) );
-			if (!(i % 10)) postMessage({ action: 'message', value: 'Feeds: ' + i + '/' + j });
-		}
-	}
+    if (importedSources) {
+        for (let i = 0, j = importedSources.length; i < j; i++) {
+            handleReq(sources.add(importedSources[i]));
+            if (!(i % 10)) postMessage({action: 'message', value: 'Feeds: ' + i + '/' + j});
+        }
+    }
 
-	if (t) {
-		for (var i=0,j=t.length; i<j; i++) {
-			handleReq( items.add(t[i]) );
-			if (!(i % 10)) postMessage({ action: 'message', value: 'Articles: ' + i + '/' + j });
-		}
-	}
+    if (importedItems) {
+        for (let i = 0, j = importedItems.length; i < j; i++) {
+            handleReq(items.add(importedItems[i]));
+            if (!(i % 10)) postMessage({action: 'message', value: 'Articles: ' + i + '/' + j});
+        }
+    }
 
-	if (writes == 0) {
-		postMessage({ action: 'finished' });
-	} else {
-		postMessage({ action: 'message', value: 'Writing...' });
-	}
+    if (writes === 0) {
+        postMessage({action: 'finished'});
+    } else {
+        postMessage({action: 'message', value: 'Writing...'});
+    }
 
 }
