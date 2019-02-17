478724c3dae8b5bfdaeb79e4f54ff3410819b7f9
zakius <jaroslaw.jarosik@outlook.com>
2019-02-17 22:39:30 +0100

    cleanup

--- scripts/app/modules/Locale.js
+++ scripts/app/modules/Locale.js
@@ -5,29 +5,29 @@
 var nl = bg.settings.get('lang') || 'en';
 define(['../../nls/' + nl, '../../nls/en'], function (lang, en) {
 
-	/**
-	 * String localization
-	 * @class Locale
-	 * @constructor
-	 * @extends Object
-	 */
-	var Locale = {
-		get c() {
-			return lang || en;
-		},
-		translate: function(str) {
-			str = String(str);
-			if (lang) return lang[str];
-			if (en) return en[str];
-			return str;
-		},
-		translateHTML: function(content) {
-			return String(content).replace(/\{\{(\w+)\}\}/gm, function(all, str) {
-				return lang[str] || en[str] || str;
-			});
-		}
-	};
+    /**
+     * String localization
+     * @class Locale
+     * @constructor
+     * @extends Object
+     */
+    var Locale = {
+        get c() {
+            return lang || en;
+        },
+        translate: function (str) {
+            str = String(str);
+            if (lang) return lang[str];
+            if (en) return en[str];
+            return str;
+        },
+        translateHTML: function (content) {
+            return String(content).replace(/\{\{(\w+)\}\}/gm, function (all, str) {
+                return lang[str] || en[str] || str;
+            });
+        }
+    };
 
-	return Locale;
+    return Locale;
 
 });
