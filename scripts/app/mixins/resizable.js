67514bb4944343dd6a099171099a75a98a94dfb7
zakius <jaroslaw.jarosik@outlook.com>
2019-02-17 22:36:13 +0100

    cleanup

--- scripts/app/mixins/resizable.js
+++ scripts/app/mixins/resizable.js
@@ -1,138 +1,137 @@
-define(['jquery'], function($) {
+define(['jquery'], function ($) {
 
-	var els = [];
+    var els = [];
 
-	var resizeWidth = bg.settings.get('thickFrameBorders') ? 10 : 6;
+    var resizeWidth = bg.settings.get('thickFrameBorders') ? 10 : 6;
 
-	function handleMouseDown(e) {
-		this.resizing = true;
-		e.preventDefault();
-		$('iframe').css('pointer-events', 'none');
-		this.trigger('resize:before');
-	}
+    function handleMouseDown(e) {
+        this.resizing = true;
+        e.preventDefault();
+        $('iframe').css('pointer-events', 'none');
+        this.trigger('resize:before');
+    }
 
-	function handleMouseMove(e) {
-		if (this.resizing) {
-			var toLeft = bg.settings.get('thickFrameBorders') ? 5 : 1;
-			e.preventDefault();
-			if (this.layout == 'vertical') {
-				setPosition.call(this, e.clientY);
-				this.$el.css('flex-basis', Math.abs(e.clientY - this.el.offsetTop + toLeft) );
-			} else {
-				setPosition.call(this, e.clientX);
-				this.$el.css('flex-basis', Math.abs(e.clientX - this.el.offsetLeft + toLeft) );
-			}
-			
-			this.trigger('resize');
-		}
-	}
+    function handleMouseMove(e) {
+        if (this.resizing) {
+            var toLeft = bg.settings.get('thickFrameBorders') ? 5 : 1;
+            e.preventDefault();
+            if (this.layout === 'vertical') {
+                setPosition.call(this, e.clientY);
+                this.$el.css('flex-basis', Math.abs(e.clientY - this.el.offsetTop + toLeft));
+            } else {
+                setPosition.call(this, e.clientX);
+                this.$el.css('flex-basis', Math.abs(e.clientX - this.el.offsetLeft + toLeft));
+            }
 
-	function handleMouseUp() {
-		if (!this.resizing) return;
-		$('iframe').css('pointer-events', 'auto');
-		this.resizing = false;
-		for (var i=0; i<els.length; i++) {
-			loadPosition.call(els[i]);
-		}
-		this.trigger('resize');
-		this.trigger('resize:after');
-	}
+            this.trigger('resize');
+        }
+    }
 
-	function setPosition(pos) {
-		var toLeft = bg.settings.get('thickFrameBorders') ? 5 : 1;
-		if (this.layout == 'vertical') {
-			this.resizer.style.width = this.$el.width() + 'px';
-			this.resizer.style.left = this.el.offsetLeft  + 'px';
-			this.resizer.style.top = pos - Math.round(resizeWidth / 2) - toLeft + 'px';
-		} else {
-			this.resizer.style.top = this.el.offsetTop + 'px';
-			this.resizer.style.left = pos - Math.round(resizeWidth / 2) - toLeft + 'px';
-		}
-	}
+    function handleMouseUp() {
+        if (!this.resizing) return;
+        $('iframe').css('pointer-events', 'auto');
+        this.resizing = false;
+        for (var i = 0; i < els.length; i++) {
+            loadPosition.call(els[i]);
+        }
+        this.trigger('resize');
+        this.trigger('resize:after');
+    }
 
-	function loadPosition(resetting) {
-		if (!this.resizer) return;
+    function setPosition(pos) {
+        var toLeft = bg.settings.get('thickFrameBorders') ? 5 : 1;
+        if (this.layout === 'vertical') {
+            this.resizer.style.width = this.$el.width() + 'px';
+            this.resizer.style.left = this.el.offsetLeft + 'px';
+            this.resizer.style.top = pos - Math.round(resizeWidth / 2) - toLeft + 'px';
+        } else {
+            this.resizer.style.top = this.el.offsetTop + 'px';
+            this.resizer.style.left = pos - Math.round(resizeWidth / 2) - toLeft + 'px';
+        }
+    }
 
-		if (this.layout == 'vertical') {
-			setPosition.call(this, this.el.offsetTop + this.el.offsetHeight);
-		} else {
-			setPosition.call(this, this.el.offsetLeft + this.el.offsetWidth);
-		}
+    function loadPosition(resetting) {
+        if (!this.resizer) return;
 
-		if (!resetting) {
-			resetPositions.call(this);
-		}
-	}
+        if (this.layout === 'vertical') {
+            setPosition.call(this, this.el.offsetTop + this.el.offsetHeight);
+        } else {
+            setPosition.call(this, this.el.offsetLeft + this.el.offsetWidth);
+        }
 
-	function resetPositions() {
-		requestAnimationFrame(function() {
-			for (var i=0; i<els.length; i++) {
-				if (els[i] == this) continue;
-				loadPosition.call(els[i], true);
-			}
-		}.bind(this));
-	}
+        if (!resetting) {
+            resetPositions.call(this);
+        }
+    }
 
-	return {
-		resizing: false,
-		resizer: null,
-		layout: 'horizontal',
-		enableResizing: function(layout, size) {
+    function resetPositions() {
+        requestAnimationFrame(function () {
+            for (var i = 0; i < els.length; i++) {
+                if (els[i] === this) continue;
+                loadPosition.call(els[i], true);
+            }
+        }.bind(this));
+    }
 
-			layout = this.layout = layout || 'horizontal';
+    return {
+        resizing: false,
+        resizer: null,
+        layout: 'horizontal',
+        enableResizing: function (layout, size) {
 
-			if (size) {
-				this.$el.css('flex-basis', size + 'px');
-			}
-			
+            layout = this.layout = layout || 'horizontal';
 
-			els.push(this);
+            if (size) {
+                this.$el.css('flex-basis', size + 'px');
+            }
 
-			var that = this;
-			if (!this.resizer) {
-				this.resizer = document.createElement('div');
-				this.resizer.className = 'resizer';
-			}
 
-			if (layout == 'vertical') {
-				//this.resizer.style.width = '100%';
-				this.resizer.style.width = this.$el.width() + 'px';
-				this.resizer.style.cursor = 'n-resize';
-				this.resizer.style.height = resizeWidth + 'px';
-			} else {
-				this.resizer.style.height = '100%';
-				this.resizer.style.cursor = 'w-resize';
-				this.resizer.style.width = resizeWidth + 'px';
-			}
+            els.push(this);
 
-			
-			loadPosition.call(this);
-			requestAnimationFrame(function() {
-				this.trigger('resize:enabled');
-			}.bind(this));
+            var that = this;
+            if (!this.resizer) {
+                this.resizer = document.createElement('div');
+                this.resizer.className = 'resizer';
+            }
 
-			this.resizer.addEventListener('mousedown', function(e) {
-				handleMouseDown.call(that, e);
-			});
-			document.addEventListener('mousemove', function(e) {
-				handleMouseMove.call(that, e);
-			});
-			document.addEventListener('mouseup', function(e) {
-				handleMouseUp.call(that, e);
-			});
-			window.addEventListener('resize', function(e) {
-				resetPositions.call(that, e);
-			});
+            if (layout === 'vertical') {
+                this.resizer.style.width = this.$el.width() + 'px';
+                this.resizer.style.cursor = 'n-resize';
+                this.resizer.style.height = resizeWidth + 'px';
+            } else {
+                this.resizer.style.height = '100%';
+                this.resizer.style.cursor = 'w-resize';
+                this.resizer.style.width = resizeWidth + 'px';
+            }
 
-			document.body.appendChild(this.resizer);
-		},
-		disableResizing: function() {
-			if (this.resizer) {
-				els.splice(els.indexOf(this), 1);
-				document.body.removeChild(this.resizer);
-				this.resizer = null;
-				resetPositions.call(this);
-			}
-		}
-	};
+
+            loadPosition.call(this);
+            requestAnimationFrame(function () {
+                this.trigger('resize:enabled');
+            }.bind(this));
+
+            this.resizer.addEventListener('mousedown', function (e) {
+                handleMouseDown.call(that, e);
+            });
+            document.addEventListener('mousemove', function (e) {
+                handleMouseMove.call(that, e);
+            });
+            document.addEventListener('mouseup', function (e) {
+                handleMouseUp.call(that, e);
+            });
+            window.addEventListener('resize', function (e) {
+                resetPositions.call(that, e);
+            });
+
+            document.body.appendChild(this.resizer);
+        },
+        disableResizing: function () {
+            if (this.resizer) {
+                els.splice(els.indexOf(this), 1);
+                document.body.removeChild(this.resizer);
+                this.resizer = null;
+                resetPositions.call(this);
+            }
+        }
+    };
 });
--- scripts/app/mixins/selectable.js
+++ scripts/app/mixins/selectable.js
@@ -1,178 +1,184 @@
-define(['jquery'], function($) {
+define(['jquery'], function ($) {
 
-return {
-	selectedItems: [],
-	selectPivot: null,
-	selectFlag: false,
-	restartSelection: function() {
-		if (this.selectedItems.length) {
-			this.selectedItems = [];
-			this.$el.find('.selected').removeClass('selected');
-			this.$el.find('.last-selected').removeClass('last-selected');
-		}
-		this.selectFirst();
-	},
-	selectFirst: function() {
-		var first = $('.' + this.itemClass + ':not(.invisible)').get(0);
-		if (first) this.select(first.view);
-	},
-	selectNext: function(e) {
-		e = e || {};
+    return {
+        selectedItems: [],
+        selectPivot: null,
+        selectFlag: false,
+        restartSelection: function () {
+            if (this.selectedItems.length) {
+                this.selectedItems = [];
+                this.$el
+                    .find('.selected')
+                    .removeClass('selected');
+                this.$el
+                    .find('.last-selected')
+                    .removeClass('last-selected');
+            }
+            this.selectFirst();
+        },
+        selectFirst: function () {
+            var first = $('.' + this.itemClass + ':not(.invisible)').get(0);
+            if (first) this.select(first.view);
+        },
+        selectNext: function (e) {
+            e = e || {};
 
-		var q = e.selectUnread ? '.unread:not(.invisible)' : '.' + this.itemClass + ':not(.invisible)';
-		var next;
-		if (e.selectUnread &&  this.selectPivot) {
-			next = this.selectPivot.el.nextElementSibling;
-		} else {
-			next = this.$el.find('.last-selected');
-			if (next.length) {
-				next = next.get(0).nextElementSibling;
-			} else {
-				next = this.el.firstElementChild;
-			}
-		}
-		while (next && !next.matchesSelector(q)) {
-			next = next.nextElementSibling;
-		}
+            const selector = e.selectUnread ? '.unread:not(.invisible)' : '.' + this.itemClass + ':not(.invisible)';
+            let next;
+            if (e.selectUnread && this.selectPivot) {
+                next = this.selectPivot.el.nextElementSibling;
+            } else {
+                next = this.$el.find('.last-selected');
+                if (next.length) {
+                    next = next.get(0).nextElementSibling;
+                } else {
+                    next = this.el.firstElementChild;
+                }
+            }
+            while (next && !next.matchesSelector(selector)) {
+                next = next.nextElementSibling;
+            }
 
-		if (!next && !e.shiftKey && !e.ctrlKey && bg.settings.get('circularNavigation')) {
-			next = this.el.querySelector(q);
-			if (e.currentIsRemoved && next && this.$el.find('.last-selected').get(0) == next) {
-				next = [];
-			}
-		}
-		if (next && next.view) {
-			this.select(next.view, e, true);
-			if (!this.inView(next)) {
-				next.scrollIntoView(false);
-			}
-		} else if (e.currentIsRemoved) {
-			app.trigger('no-items:' + this.el.id);
-		}
+            if (!next && !e.shiftKey && !e.ctrlKey && bg.settings.get('circularNavigation')) {
+                next = this.el.querySelector(selector);
+                if (e.currentIsRemoved && next && this.$el.find('.last-selected').get(0) === next) {
+                    next = [];
+                }
+            }
+            if (next && next.view) {
+                this.select(next.view, e, true);
+                if (!this.inView(next)) {
+                    next.scrollIntoView(false);
+                }
+            } else if (e.currentIsRemoved) {
+                app.trigger('no-items:' + this.el.id);
+            }
 
-	},
-	selectPrev: function(e) {
-		e = e || {};
-		var q = e.selectUnread ? '.unread:not(.invisible)' : '.' + this.itemClass + ':not(.invisible)';
-		var prev;
-		if (e.selectUnread &&  this.selectPivot) {
-			prev = this.selectPivot.el.previousElementSibling;
-		} else {
-			prev = this.$el.find('.last-selected');
-			if (prev.length) {
-				prev = prev.get(0).previousElementSibling;
-			} else {
-				prev = this.el.lastElementChild;
-			}
-		}
-		while (prev && !prev.matchesSelector(q)) {
-			prev = prev.previousElementSibling;
-		}
+        },
+        selectPrev: function (e) {
+            e = e || {};
+            const selector = e.selectUnread ? '.unread:not(.invisible)' : '.' + this.itemClass + ':not(.invisible)';
+            let prev;
+            if (e.selectUnread && this.selectPivot) {
+                prev = this.selectPivot.el.previousElementSibling;
+            } else {
+                prev = this.$el.find('.last-selected');
+                if (prev.length) {
+                    prev = prev.get(0).previousElementSibling;
+                } else {
+                    prev = this.el.lastElementChild;
+                }
+            }
+            while (prev && !prev.matchesSelector(selector)) {
+                prev = prev.previousElementSibling;
+            }
 
-		if (!prev && !e.shiftKey && !e.ctrlKey && bg.settings.get('circularNavigation')) {
-			prev = this.$el.find(q + ':last').get(0);
-			if (e.currentIsRemoved && prev && this.$el.find('.last-selected').get(0) == prev) {
-				prev = [];
-			}
-		}
-		if (prev && prev.view) {
-			this.select(prev.view, e, true);
-			if (!this.inView(prev)) {
-				prev.scrollIntoView(true);
-			}
-		} else if (e.currentIsRemoved) {
-			app.trigger('no-items:' + this.el.id);
-		}
-	},
-	select: function(view, e, forceSelect) {
-		e = e || {};
-		var that = this;
-		if ( (e.shiftKey != true && e.ctrlKey != true) || (e.shiftKey && !this.selectPivot)) {
-			this.selectedItems = [];
-			this.selectPivot = view;
-			this.$el.find('.selected').removeClass('selected');
+            if (!prev && !e.shiftKey && !e.ctrlKey && bg.settings.get('circularNavigation')) {
+                prev = this.$el.find(selector + ':last').get(0);
+                if (e.currentIsRemoved && prev && this.$el.find('.last-selected').get(0) === prev) {
+                    prev = [];
+                }
+            }
+            if (prev && prev.view) {
+                this.select(prev.view, e, true);
+                if (!this.inView(prev)) {
+                    prev.scrollIntoView(true);
+                }
+            } else if (e.currentIsRemoved) {
+                app.trigger('no-items:' + this.el.id);
+            }
+        },
+        select: function (view, e, forceSelect) {
+            e = e || {};
+            let that = this;
+            if ((e.shiftKey !== true && e.ctrlKey !== true) || (e.shiftKey && !this.selectPivot)) {
+                this.selectedItems = [];
+                this.selectPivot = view;
+                this.$el.find('.selected').removeClass('selected');
 
 
-			if (!window || !window.frames) {
-				bg.logs.add({ message: 'Event duplication bug! Clearing events now...' });
-				bg.console.log('Event duplication bug! Clearing events now...');
-				bg.sources.trigger('clear-events', -1);
-				return;
-			}
+                if (!window || !window.frames) {
+                    bg.logs.add({message: 'Event duplication bug! Clearing events now...'});
+                    bg.console.log('Event duplication bug! Clearing events now...');
+                    bg.sources.trigger('clear-events', -1);
+                    return;
+                }
 
-			setTimeout(function() {
-				this.trigger('pick', view, e);
-			}.bind(this), 0);
+                setTimeout(function () {
+                    this.trigger('pick', view, e);
+                }.bind(this), 0);
 
-		} else if (e.shiftKey && this.selectPivot) {
-			this.$el.find('.selected').removeClass('selected');
-			this.selectedItems = [this.selectPivot];
-			this.selectedItems[0].$el.addClass('selected');
+            } else if (e.shiftKey && this.selectPivot) {
+                this.$el.find('.selected').removeClass('selected');
+                this.selectedItems = [this.selectPivot];
+                this.selectedItems[0].$el.addClass('selected');
 
-			if (this.selectedItems[0] != view) {
-				if (this.selectedItems[0].$el.index() < view.$el.index() ) {
-					this.selectedItems[0].$el.nextUntil(view.$el).not('.invisible,.date-group').each(function(i, el) {
-						$(el).addClass('selected');
-						that.selectedItems.push(el.view);
-					});
-				} else {
-					view.$el.nextUntil(this.selectedItems[0].$el).not('.invisible,.date-group').each(function(i, el) {
-						$(el).addClass('selected');
-						that.selectedItems.push(el.view);
-					});
-				}
+                if (this.selectedItems[0] !== view) {
+                    if (this.selectedItems[0].$el.index() < view.$el.index()) {
+                        this.selectedItems[0].$el
+                            .nextUntil(view.$el)
+                            .not('.invisible,.date-group')
+                            .each(function (i, el) {
+                                $(el).addClass('selected');
+                                that.selectedItems.push(el.view);
+                            });
+                    } else {
+                        view.$el
+                            .nextUntil(this.selectedItems[0].$el)
+                            .not('.invisible,.date-group')
+                            .each(function (i, el) {
+                                $(el).addClass('selected');
+                                that.selectedItems.push(el.view);
+                            });
+                    }
 
-			}
+                }
 
-			if (forceSelect === true) {
-				setTimeout(function() {
-					this.trigger('pick', view, e);
-				}.bind(this), 0);
-			}
+                if (forceSelect === true) {
+                    setTimeout(function () {
+                        this.trigger('pick', view, e);
+                    }.bind(this), 0);
+                }
 
-		} else if (e.ctrlKey && view.$el.hasClass('selected')) {
-			view.$el.removeClass('selected');
-			view.$el.removeClass('last-selected');
-			this.selectPivot = null;
-			this.selectedItems.splice(this.selectedItems.indexOf(view), 1);
-			return;
-		} else if (e.ctrlKey) {
-			this.selectPivot = view;
-		}
+            } else if (e.ctrlKey && view.$el.hasClass('selected')) {
+                view.$el.removeClass('selected');
+                view.$el.removeClass('last-selected');
+                this.selectPivot = null;
+                this.selectedItems.splice(this.selectedItems.indexOf(view), 1);
+                return;
+            } else if (e.ctrlKey) {
+                this.selectPivot = view;
+            }
 
-		this.$el.find('.last-selected').removeClass('last-selected');
-		if (this.selectedItems[0] != view) {
-			this.selectedItems.push(view);
-			view.$el.addClass('selected');
-		}
-		view.$el.addClass('last-selected');
-	},
-	inView: function(cel) {
-		var $cel = $(cel);
-		if ($cel.position().top - this.$el.offset().top < 0 || $cel.position().top + cel.offsetHeight >= this.el.offsetHeight) {
-			return false;
-		}
-		return true;
-	},
-	handleSelectableMouseDown: function(e) {
-		if(e.which === 2){
-			return browser.tabs.create({url: e.currentTarget.view.model.attributes.url, active: false});
-		}
-		var item = e.currentTarget.view;
-		if (this.selectedItems.length > 1 && item.$el.hasClass('selected') && !e.ctrlKey && !e.shiftKey) {
-			this.selectFlag = true;
-			return;
-		}
-		// used to be just { shiftKey: e.shiftKey, ctrlKey: e.ctrlKey } instead of 'e'
-		this.select(item, e);
-	},
-	handleSelectableMouseUp: function(e) {
-		var item = e.currentTarget.view;
-		if (e.which == 1 && this.selectedItems.length > 1 && this.selectFlag) {
-			this.select(item, e);
-			this.selectFlag = false;
-		}
-	}
+            this.$el.find('.last-selected').removeClass('last-selected');
+            if (this.selectedItems[0] !== view) {
+                this.selectedItems.push(view);
+                view.$el.addClass('selected');
+            }
+            view.$el.addClass('last-selected');
+        },
+        inView: function (cel) {
+            var $cel = $(cel);
+            return !($cel.position().top - this.$el.offset().top < 0 || $cel.position().top + cel.offsetHeight >= this.el.offsetHeight);
 
-};
+        },
+        handleSelectableMouseDown: function (e) {
+            if (e.which === 2) {
+                return browser.tabs.create({url: e.currentTarget.view.model.attributes.url, active: false});
+            }
+            const item = e.currentTarget.view;
+            if (this.selectedItems.length > 1 && item.$el.hasClass('selected') && !e.ctrlKey && !e.shiftKey) {
+                this.selectFlag = true;
+                return;
+            }
+            this.select(item, e);
+        },
+        handleSelectableMouseUp: function (e) {
+            const item = e.currentTarget.view;
+            if (e.which === 1 && this.selectedItems.length > 1 && this.selectFlag) {
+                this.select(item, e);
+                this.selectFlag = false;
+            }
+        }
+    };
 });
