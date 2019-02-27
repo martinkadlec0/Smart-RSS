5ea4f313f32f47f81401348c96b7f6d1f03fb35b
zakius <jaroslaw.jarosik@outlook.com>
2019-02-27 17:34:21 +0100

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
@@ -7,23 +7,23 @@ module.exports = function(grunt) {
 
         jshint: {
             options: {
-                curly:    false, // true: force { }
-                eqnull:   true,  // true: enable something == null
-                eqeqeq:   true, // true: force ===
-                immed:    true,  // true: immidiatly invocated fns has to be in ()
-                newcap:   true,  // true: construcotr has to have firt letter uppercased
-                noarg:    true,  // true: no arguments.caller and arguments.callee
-                sub:      true,  // true: no warning about a['something'] if a.something can be used
-                undef:    true,  // true: can't use undeclared vars
-                browser:  true,  // true: set window object and other stuff as globals
-                devel:    true,  // true: set alert,confirm,console,... as globals
-                boss:     true,  // true: allow assigments in conditions and return statements
-                forin:    true,  // true: hasOwnProperty has to be in all for..in cycles
-                noempty:  true,  // true: no empty blocks
-                unused:   true,  // true: warn about unused vars
+                curly: false, // true: force { }
+                eqnull: true,  // true: enable something == null
+                eqeqeq: true, // true: force ===
+                immed: true,  // true: immidiatly invocated fns has to be in ()
+                newcap: true,  // true: construcotr has to have firt letter uppercased
+                noarg: true,  // true: no arguments.caller and arguments.callee
+                sub: true,  // true: no warning about a['something'] if a.something can be used
+                undef: true,  // true: can't use undeclared vars
+                browser: true,  // true: set window object and other stuff as globals
+                devel: true,  // true: set alert,confirm,console,... as globals
+                boss: true,  // true: allow assigments in conditions and return statements
+                forin: true,  // true: hasOwnProperty has to be in all for..in cycles
+                noempty: true,  // true: no empty blocks
+                unused: true,  // true: warn about unused vars
                 trailing: true,  // true: no trailing whitespaces
                 supernew: true,  // true: enable 'new Constructor' instead of 'new Constructor()'
-                onevar:   false, // true: only one var per fn
+                onevar: false, // true: only one var per fn
                 funcscope: false,   // false: no 'var' in blocks
                 maxdepth: 5,        // max nesting depth
                 quotmark: 'single', // single: force '
@@ -48,87 +48,41 @@ module.exports = function(grunt) {
             all: ['scripts/app/**/*.js', 'scripts/bgprocess/**/*.js']
         },
 
-        requirejs: {
-            app: {
-                options: {
-                    name: '../main',
-                    baseUrl: 'scripts/app',
-                    generateSourceMaps: true,
-                    preserveLicenseComments: false,
-                    optimize: 'none',
-                    waitSeconds: 0,
-                    paths: {
-                        jquery: '../libs/jquery.min',
-                        underscore: '../libs/underscore.min',
-                        backbone: '../libs/backbone.min',
-                        text: '../text',
-                        i18n: '../i18n',
-                        domReady: '../domReady'
-                    },
-                    shim: {
-                        jquery: {
-                            exports: '$'
-                        },
-                        backbone: {
-                            deps: ['underscore', 'jquery'],
-                            exports: 'Backbone'
-                        },
-                        underscore: {
-                            exports: '_'
-                        }
-                    },
-                    excludeShallow: ['modules/Locale', 'jquery', 'underscore', 'backbone'],
-                    out: 'scripts/main-compiled.js'
-                }
-            },
-            bg: {
-                options: {
-                    name: '../bgprocess',
-                    baseUrl: 'scripts/bgprocess',
-                    generateSourceMaps: true,
-                    preserveLicenseComments: false,
-                    optimize: 'none',
-                    waitSeconds: 0,
-                    paths: {
-                        jquery: '../libs/jquery.min',
-                        underscore: '../libs/underscore.min',
-                        backbone: '../libs/backbone.min',
-                        text: '../text',
-                        i18n: '../i18n',
-                        md5: '../libs/md5',
-                        domReady: '../domReady',
-                        backboneDB: '../libs/backbone.indexDB'
-                    },
-                    shim: {
-                        jquery: {
-                            exports: '$'
-                        },
-                        backboneDB: {
-                            deps: ['backbone']
-                        },
-                        backbone: {
-                            deps: ['underscore', 'jquery'],
-                            exports: 'Backbone'
-                        },
-                        underscore: {
-                            exports: '_'
-                        },
-                        md5: {
-                            exports: 'CryptoJS'
-                        }
-                    },
-                    excludeShallow: ['jquery', 'underscore', 'backbone', 'backboneDB'],
-                    out: 'scripts/bgprocess-compiled.js'
-                }
+        zip: {
+            'skip-files': {
+                router: function (filepath) {
+                    var skipped = [
+                        'node_modules',
+                        'package.json',
+                        'package-lock.json',
+                        'package.zip'
+                    ];
+                    var path = require('path');
+                    var name = path.basename(filepath);
+                    if (skipped.includes(name)) {
+                        return null;
+                    }
+                    if(filepath.includes('node_modules')){
+                        return null;
+                    }
+                    console.log(name);
+
+
+                    return filepath;
+
+
+                },
+                src: ['**/*'],
+                dest: 'package.zip',
             }
-        },
+        }
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-zip');
 
     // Default task(s).
     grunt.registerTask('default', ['jshint']);
-    grunt.registerTask('rjs', ['requirejs:app', 'requirejs:bg']);
+    grunt.registerTask('package', ['zip']);
 };
