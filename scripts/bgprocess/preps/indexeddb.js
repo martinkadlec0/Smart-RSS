c5e0d5253efa74e2f71ccd69d11f6beec8deecd0
zakius <jaroslaw.jarosik@outlook.com>
2019-03-11 22:45:04 +0100

    formatting

--- scripts/bgprocess/preps/indexeddb.js
+++ scripts/bgprocess/preps/indexeddb.js
@@ -3,35 +3,39 @@
  * @module BgProcess
  * @submodule preps/indexeddb
  */
-define(['backbone', 'backboneDB'], function(BB) {
+define(['backbone', 'backboneDB'], function (BB) {
 
-	/**
-	 * IndexedDB preps.
-	 */
+    /**
+     * IndexedDB preps.
+     */
 
-	BB.LocalStorage.prepare = function(db) {
-		if (!db.objectStoreNames.contains('settings-backbone'))
-			db.createObjectStore('settings-backbone', { keyPath: 'id' });
+    BB.LocalStorage.prepare = function (db) {
+        if (!db.objectStoreNames.contains('settings-backbone')) {
+            db.createObjectStore('settings-backbone', {keyPath: 'id'});
+        }
 
-		if (!db.objectStoreNames.contains('items-backbone'))
-			db.createObjectStore('items-backbone',    { keyPath: 'id' });
+        if (!db.objectStoreNames.contains('items-backbone')) {
+            db.createObjectStore('items-backbone', {keyPath: 'id'});
+        }
 
-		if (!db.objectStoreNames.contains('sources-backbone'))
-			db.createObjectStore('sources-backbone',  { keyPath: 'id' });
+        if (!db.objectStoreNames.contains('sources-backbone')) {
+            db.createObjectStore('sources-backbone', {keyPath: 'id'});
+        }
 
-		if (!db.objectStoreNames.contains('folders-backbone'))
-			db.createObjectStore('folders-backbone', { keyPath: 'id' });
+        if (!db.objectStoreNames.contains('folders-backbone')) {
+            db.createObjectStore('folders-backbone', {keyPath: 'id'});
+        }
 
-		if (!db.objectStoreNames.contains('toolbars-backbone'))
-			db.createObjectStore('toolbars-backbone', { keyPath: 'region' });
+        if (!db.objectStoreNames.contains('toolbars-backbone')) {
+            db.createObjectStore('toolbars-backbone', {keyPath: 'region'});
+        }
+    };
 
-	};
+    /**
+     * 1 -> 3: Main objects stores and testing
+     * 3 -> 4: Added toolbars-backbone store
+     */
+    BB.LocalStorage.version = 4;
 
-	/**
-	 * 1 -> 3: Main objects stores and testing
-	 * 3 -> 4: Added toolbars-backbone store
-	 */
-	BB.LocalStorage.version = 4;
-
-	return true;
+    return true;
 });
