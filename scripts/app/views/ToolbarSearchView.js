79e7a2de21010e4afe3f67d4f8074e87c280775f
zakius <jaroslaw.jarosik@outlook.com>
2019-03-12 00:01:49 +0100

    allow selecting text in search field

--- scripts/app/views/ToolbarSearchView.js
+++ scripts/app/views/ToolbarSearchView.js
@@ -1,27 +1,22 @@
 define(['backbone', 'jquery', 'modules/Locale', 'domReady!'], function (BB, $, Locale) {
-	var ToolbarSearchView = BB.View.extend({
-		tagName: 'div',
-		className: 'button',
-		initialize: function() {
+    return BB.View.extend({
+        tagName: 'div',
+        className: 'button',
+        initialize: function () {
 
-			var action = app.actions.get(this.model.get('actionName'));
+            const action = app.actions.get(this.model.get('actionName'));
 
-			var newEl = $('<input type="search" required class="input-search" />');
-			this.$el.replaceWith(newEl);
-			this.$el = newEl;
-			this.$el.attr('placeholder', Locale.SEARCH);
-			this.$el.attr('tabindex', -1);
-			this.el = this.$el.get(0);
+            const newEl = $('<input type="search" required class="input-search" />');
+            this.$el.replaceWith(newEl);
+            this.$el = newEl;
+            this.$el.attr('placeholder', Locale.SEARCH);
+            this.$el.attr('tabindex', -1);
+            this.el = this.$el.get(0);
 
-			this.el.dataset.action = this.model.get('actionName');
-			this.el.title = action.get('title');
+            this.el.dataset.action = this.model.get('actionName');
+            this.el.title = action.get('title');
 
-
-			this.el.setAttribute('draggable', 'true');
-
-			this.el.view = this;
-		}
-	});
-
-	return ToolbarSearchView;
+            this.el.view = this;
+        }
+    });
 });
