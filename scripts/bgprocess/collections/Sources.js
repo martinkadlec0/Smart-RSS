57834b6fde1e7de4715e6575d949cbd4862684ad
zakius <jaroslaw.jarosik@outlook.com>
2019-03-28 22:56:05 +0100

    cleanup

--- scripts/bgprocess/collections/Sources.js
+++ scripts/bgprocess/collections/Sources.js
@@ -4,22 +4,19 @@
  */
 define(['backbone', 'models/Source', 'preps/indexeddb'], function (BB, Source) {
 
-	/**
-	 * Collection of feed modules
-	 * @class Sources
-	 * @constructor
-	 * @extends Backbone.Collection
-	 */
-	var Sources = BB.Collection.extend({
-		model: Source,
-		localStorage: new Backbone.LocalStorage('sources-backbone'),
-		comparator: function(a, b) {
-			var t1 = (a.get('title') || '').trim().toLowerCase();
-			var t2 = (b.get('title') || '').trim().toLowerCase();
-			return t1 < t2  ? -1 : 1;
-		}
-	});
-
-	return Sources;
-
+    /**
+     * Collection of feed modules
+     * @class Sources
+     * @constructor
+     * @extends Backbone.Collection
+     */
+    return BB.Collection.extend({
+        model: Source,
+        localStorage: new Backbone.LocalStorage('sources-backbone'),
+        comparator: function (a, b) {
+            const t1 = (a.get('title') || '').trim().toLowerCase();
+            const t2 = (b.get('title') || '').trim().toLowerCase();
+            return t1 < t2 ? -1 : 1;
+        }
+    });
 });
