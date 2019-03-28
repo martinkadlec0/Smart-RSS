8fe836d081adfae0d07d8a804d04b820c468394f
zakius <jaroslaw.jarosik@outlook.com>
2019-03-28 22:55:26 +0100

    cleanup

--- scripts/bgprocess/collections/Items.js
+++ scripts/bgprocess/collections/Items.js
@@ -14,23 +14,29 @@ define(['backbone', 'models/Item', 'preps/indexeddb'], function (BB, Item) {
      * @constructor
      * @extends Backbone.Collection
      */
-    var Items = BB.Collection.extend({
+    let Items = BB.Collection.extend({
         model: Item,
         batch: false,
         localStorage: new Backbone.LocalStorage('items-backbone'),
         comparator: function (a, b, sorting) {
-            var val;
+            let val;
             sortBy = sorting || settings.get('sortBy');
 
 
             if (sortBy === 'title') {
-                if (!sorting && getS(a.get('title')) === getS(b.get('title'))) return this.comparator(a, b, settings.get('sortBy2') || true);
+                if (!sorting && getS(a.get('title')) === getS(b.get('title'))) {
+                    return this.comparator(a, b, settings.get('sortBy2') || true);
+                }
                 val = getS(a.get('title')) <= getS(b.get('title')) ? 1 : -1;
             } else if (sortBy === 'author') {
-                if (!sorting && getS(a.get('author')) === getS(b.get('author'))) return this.comparator(a, b, settings.get('sortBy2') || true);
+                if (!sorting && getS(a.get('author')) === getS(b.get('author'))) {
+                    return this.comparator(a, b, settings.get('sortBy2') || true);
+                }
                 val = getS(a.get('author')) <= getS(b.get('author')) ? 1 : -1;
             } else {
-                if (!sorting && a.get('date') === b.get('date')) return this.comparator(a, b, settings.get('sortBy2') || true);
+                if (!sorting && a.get('date') === b.get('date')) {
+                    return this.comparator(a, b, settings.get('sortBy2') || true);
+                }
                 val = a.get('date') <= b.get('date') ? 1 : -1;
             }
 
@@ -43,13 +49,13 @@ define(['backbone', 'models/Item', 'preps/indexeddb'], function (BB, Item) {
             return val;
         },
         initialize: function () {
-            //settings.on('change:sortOrder', this.sort, this);
             this.listenTo(settings, 'change:sortOrder', this.sort);
             this.listenTo(settings, 'change:sortOrder2', this.sort);
             this.listenTo(settings, 'change:sortBy', this.sort);
             this.listenTo(settings, 'change:sortBy2', this.sort);
         }
     });
+
 
     return Items;
 
