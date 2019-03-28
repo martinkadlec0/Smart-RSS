2f9be5f80980bc7ad6139a93988413c949c2911a
zakius <jaroslaw.jarosik@outlook.com>
2019-03-28 22:53:15 +0100

    cleanup

--- scripts/bgprocess/collections/Folders.js
+++ scripts/bgprocess/collections/Folders.js
@@ -4,22 +4,19 @@
  */
 define(['backbone', 'models/Folder', 'preps/indexeddb'], function (BB, Folder) {
 
-	/**
-	 * Collection of feed folders
-	 * @class Folders
-	 * @constructor
-	 * @extends Backbone.Collection
-	 */
-	var Folders = BB.Collection.extend({
-		model: Folder,
-		localStorage: new Backbone.LocalStorage('folders-backbone'),
-		comparator: function(a, b) {
-			var t1 = (a.get('title') || '').trim().toLowerCase();
-			var t2 = (b.get('title') || '').trim().toLowerCase();
-			return t1 < t2  ? -1 : 1;
-		}
-	});
-
-	return Folders;
-
+    /**
+     * Collection of feed folders
+     * @class Folders
+     * @constructor
+     * @extends Backbone.Collection
+     */
+    return BB.Collection.extend({
+        model: Folder,
+        localStorage: new Backbone.LocalStorage('folders-backbone'),
+        comparator: function (a, b) {
+            const t1 = (a.get('title') || '').trim().toLowerCase();
+            const t2 = (b.get('title') || '').trim().toLowerCase();
+            return t1 < t2 ? -1 : 1;
+        }
+    });
 });
