a37eed11ca9dcbbb1fe8a158b61cfebc41ac6685
zakius <jaroslaw.jarosik@outlook.com>
2019-04-16 13:54:39 +0200

    Cleanup

--- scripts/app/preps/extendNative.js
+++ scripts/app/preps/extendNative.js
@@ -7,49 +7,6 @@
 define([], function () {
 
     /**
-     * Set or get last array item
-     * @method last
-     * @extends Array
-     * @param value {Any} Value to set - optional
-     * @return {Any} Last item of array, null if array is empty
-     */
-    Array.prototype.last = function (value) {
-        if (!this.length) {
-            return null;
-        }
-        if (value) {
-            this[this.length - 1] = value;
-        }
-        return this[this.length - 1];
-    };
-
-    /**
-     * Set or get first array item
-     * @method last
-     * @extends Array
-     * @param value {Any} Value to set - optional
-     * @return {Any} First item of array, null if array is empty
-     */
-    Array.prototype.first = function (value) {
-        if (!this.length) {
-            return null;
-        }
-        if (value) {
-            this[0] = value;
-        }
-        return this[0];
-    };
-
-    /**
-     * Get index of element in HTMLCollection (used by eg. Element#children)
-     * @method indexOf
-     * @extends HTMLCollection
-     * @param element {HTMLElement} Element fo find index of
-     * @return {Any} First item of array, null if array is empty
-     */
-    HTMLCollection.prototype.indexOf = Array.prototype.indexOf;
-
-    /**
      * Escapes regexp characters in string
      * @method escape
      * @extends RegExp
