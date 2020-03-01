a488b390e1fac807def4b27d05dd5511cec61e64
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2020-03-01 20:57:17 +0100

    Prevent search field from being marked red
    remove `required` attribute

--- src/scripts/app/views/ToolbarSearchView.js
+++ src/scripts/app/views/ToolbarSearchView.js
@@ -7,7 +7,6 @@ define(['backbone', 'modules/Locale'], function (BB, Locale) {
             this.el.setAttribute('placeholder', Locale.SEARCH);
             this.el.setAttribute('type', 'search');
             this.el.setAttribute('tabindex', -1);
-            this.el.required = true;
 
             const action = app.actions.get(this.model.get('actionName'));
 
