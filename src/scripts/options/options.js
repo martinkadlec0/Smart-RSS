ae5f6a18b5eabbf995a4617685a218692eeb6242
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2020-03-15 22:47:23 +0100

    Add special keys map to options

--- src/scripts/options/options.js
+++ src/scripts/options/options.js
@@ -6,6 +6,28 @@ const entityMap = {
     '\'': '&#39;'
 };
 
+
+const keys = {
+    8: 'backspace',
+    9: 'tab',
+    13: 'enter',
+    //16: 'shift',
+    //17: 'ctrl',
+    20: 'capslock',
+    27: 'esc',
+    32: 'space',
+    33: 'pgup',
+    34: 'pgdown',
+    35: 'end',
+    36: 'home',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    45: 'insert',
+    46: 'del'
+};
+
 function escapeHtml(string) {
     return String(string).replace(/[&<>"']/gm, (s) => {
         return entityMap[s];
