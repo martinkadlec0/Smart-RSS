1e20c49bc0c734642d1f486cb78ee977cd0ae222
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2020-06-20 14:43:11 +0200

    Cleanup

--- src/scripts/app/views/Properties.js
+++ src/scripts/app/views/Properties.js
@@ -9,15 +9,12 @@ define([
             template: Locale.translateHTML(`
 <label id="property-title-label">
   {{NAME}}:
-  <input id="prop-title" placeholder="{{FETCH_TITLE_TIP}}" title="{{FETCH_TITLE_TIP}}" type="text" value="<%- title %>"/>
+  <input id="prop-title" placeholder="{{FETCH_TITLE_TIP}}" title="{{FETCH_TITLE_TIP}}" type="text" value=""/>
 </label>
 
-<label id="property-title-address">{{ADDRESS}}: <input id="prop-url" type="url" value="<%= url %>"/></label>
+<label id="property-title-address">{{ADDRESS}}: <input id="prop-url" type="url" value=""/></label>
 
 <label>{{UPDATE}}: <select id="prop-update-every">
-  <% if (typeof updateEveryDiffers != 'undefined' && updateEveryDiffers == true) { %>
-  <option value="-2">&lt;mixed&gt;</option>
-  <% } %>
   <option value="-1">Use global setting</option>
   <option value="0">{{NEVER}}</option>
   <option value="5">{{EVERY_5_MINUTES}}</option>
@@ -34,17 +31,11 @@ define([
 
 
 <label>{{PARENT}}: <select id="prop-parent">
-  <% if (typeof folderIdDiffers != 'undefined' && folderIdDiffers == true) { %>
-  <option value="-2">&lt;mixed&gt;</option>
-  <% } %>
   <option value="0">{{ROOT_FOLDER}}</option>
 </select></label>
 
 
 <label>{{AUTOREMOVE}}: <select id="prop-autoremove">
-  <% if (typeof autoremoveDiffers != 'undefined' && autoremoveDiffers == true) { %>
-  <option value="-2">&lt;mixed&gt;</option>
-  <% } %>
   <option value="0">{{NEVER}}</option>
   <option value="1">{{OLDER_THAN_DAY}}</option>
   <option value="7">{{OLDER_THAN_WEEK}}</option>
@@ -145,35 +136,9 @@ define([
 
                 const fragment = document.createRange().createContextualFragment(this.template);
 
-                const labelTitle = fragment.querySelector('#property-title-label');
-                if (!properties.title) {
-                    fragment.removeChild(labelTitle);
-                } else {
-                    labelTitle.querySelector('input').value = properties.title;
-                }
-
-
-                if (properties.updateEveryDiffers === true) {
-                    const option = document.createRange().createContextualFragment(`<option value="-2">&lt;mixed&gt;</option>`);
-                    fragment.querySelector('#prop-update-every').prepend(option);
-                }
-
-                if (properties.folderIdDiffers === true) {
-                    const option = document.createRange().createContextualFragment(`<option value="-2">&lt;mixed&gt;</option>`);
-                    fragment.querySelector('#prop-parent').prepend(option);
-                }
-
-                if (properties.autoremoveDiffers === true) {
-                    const option = document.createRange().createContextualFragment(`<option value="-2">&lt;mixed&gt;</option>`);
-                    fragment.querySelector('#prop-autoremove').prepend(option);
-                }
-
-                const labelUrl = fragment.querySelector('#property-title-address');
-                if (!properties.title) {
-                    fragment.removeChild(labelUrl);
-                } else {
-                    labelUrl.querySelector('input').value = properties.url;
-                    const details = document.createRange().createContextualFragment(Locale.translateHTML(`<details>
+                fragment.querySelector('#property-title-label input').value = properties.title;
+                fragment.querySelector('#property-title-address input').value = properties.url;
+                const details = document.createRange().createContextualFragment(Locale.translateHTML(`<details>
   <summary>{{MORE}}</summary>
   <label>{{USERNAME}}: <input id="prop-username" type="text" value=""/></label>
   <label>{{PASSWORD}}: <input id="prop-password" type="password" value=""/></label>
@@ -185,13 +150,11 @@ define([
     <option value="no">No</option>
   </select></label>
 </details>`));
-                    details.querySelector('#prop-username').value = properties.username;
-                    details.querySelector('#prop-password').value = properties.password;
-                    details.querySelector('#prop-proxy').value = properties.proxyThroughFeedly;
+                details.querySelector('#prop-username').value = properties.username;
+                details.querySelector('#prop-password').value = properties.password;
+                details.querySelector('#prop-proxy').value = properties.proxyThroughFeedly;
 
-                    fragment.insertBefore(details, fragment.querySelector('button'));
-                }
-
+                fragment.insertBefore(details, fragment.querySelector('button'));
 
                 this.el.appendChild(fragment);
 
@@ -232,19 +195,48 @@ define([
 
                 const properties = isFolder ? Object.assign(params, this.current.attributes) : params;
 
-                const fragment = document.createRange().createContextualFragment(this.template);
-
                 /**
                  * Test if all selected feeds has the same properties or if they are mixed
                  */
 
-                const labelTitle = fragment.querySelector('#property-title-label');
-                if (!properties.title) {
-                    fragment.removeChild(labelTitle);
-                } else {
-                    labelTitle.querySelector('input').value = properties.title;
-                }
+                params.firstUpdate = listOfSources[0].get('updateEvery');
+                params.updateEveryDiffers = listOfSources.some(function (c) {
+                    if (params.firstUpdate !== c.get('updateEvery')) {
+                        return true;
+                    }
+                });
 
+                params.firstAutoremove = listOfSources[0].get('autoremove');
+                params.autoremoveDiffers = listOfSources.some(function (c) {
+                    if (params.firstAutoremove !== c.get('autoremove')) {
+                        return true;
+                    }
+                });
+
+                params.firstFolderId = listOfSources[0].get('folderID');
+                params.folderIdDiffers = listOfSources.some(function (c) {
+                    if (params.firstAutoremove !== c.get('folderID')) {
+                        return true;
+                    }
+                });
+
+                /**
+                 * Create HTML
+                 */
+                const fragment = document.createRange().createContextualFragment(this.template);
+
+                const labelTitle = fragment.querySelector('#property-title-label');
+                if (properties.title) {
+                    labelTitle.querySelector('input').value = properties.title;
+                } else {
+                    fragment.removeChild(labelTitle);
+                }
+                const labelUrl = fragment.querySelector('#property-title-address');
+                if (properties.url) {
+                    labelUrl.querySelector('input').value = properties.url;
+                } else {
+                    fragment.removeChild(labelUrl);
+                }
 
                 if (properties.updateEveryDiffers === true) {
                     const option = document.createRange().createContextualFragment(`<option value="-2">&lt;mixed&gt;</option>`);
@@ -261,47 +253,9 @@ define([
                     fragment.querySelector('#prop-autoremove').prepend(option);
                 }
 
-                const labelUrl = fragment.querySelector('#property-title-address');
-                if (!properties.title) {
-                    fragment.removeChild(labelUrl);
-                }
-
-                if (!properties.url) {
-                    fragment.removeChild(labelUrl);
-                }
-
-                if (listOfSources.length) {
-                    params.firstUpdate = listOfSources[0].get('updateEvery');
-                    params.updateEveryDiffers = listOfSources.some(function (c) {
-                        if (params.firstUpdate !== c.get('updateEvery')) {
-                            return true;
-                        }
-                    });
-
-                    params.firstAutoremove = listOfSources[0].get('autoremove');
-                    params.autoremoveDiffers = listOfSources.some(function (c) {
-                        if (params.firstAutoremove !== c.get('autoremove')) {
-                            return true;
-                        }
-                    });
-
-                    params.firstFolderId = listOfSources[0].get('folderID');
-                    params.folderIdDiffers = listOfSources.some(function (c) {
-                        if (params.firstAutoremove !== c.get('folderID')) {
-                            return true;
-                        }
-                    });
-                }
-
-                /**
-                 * Create HTML
-                 */
-
-
-                this.el.appendChild(fragment);
 
                 const folders = bg.folders;
-                const parentSelect = document.querySelector('#prop-parent');
+                const parentSelect = fragment.querySelector('#prop-parent');
                 folders.forEach((folder) => {
                     const option = document.createElement('option');
                     option.textContent = folder.get('title');
@@ -314,16 +268,16 @@ define([
                  */
 
                 if (!params.autoremoveDiffers) {
-                    document.querySelector('#prop-autoremove').value = params.firstAutoremove;
+                    fragment.querySelector('#prop-autoremove').value = params.firstAutoremove;
                 }
                 if (!params.updateEveryDiffers) {
-                    document.querySelector('#prop-update-every').value = params.firstUpdate;
+                    fragment.querySelector('#prop-update-every').value = params.firstUpdate;
                 }
 
                 if (!params.folderIdDiffers) {
-                    document.querySelector('#prop-parent').value = params.firstFolderId;
+                    fragment.querySelector('#prop-parent').value = params.firstFolderId;
                 }
-
+                this.el.appendChild(fragment);
             },
 
             render: function () {
