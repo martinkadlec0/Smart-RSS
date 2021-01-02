1eb273435ed9c458e520a29960259cb9493595c4
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2021-01-02 20:46:59 +0100

    Cleanup

--- src/scripts/app/views/ToolbarButtonView.js
+++ src/scripts/app/views/ToolbarButtonView.js
@@ -3,14 +3,11 @@ define(['backbone'], function (BB) {
         tagName: 'div',
         className: 'button',
         initialize: function () {
-
             const action = app.actions.get(this.model.get('actionName'));
             this.el.style.background = 'url("/images/' + action.get('icon') + '") no-repeat center center';
 
             this.el.dataset.action = this.model.get('actionName');
             this.el.title = action.get('title');
-
-            this.el.setAttribute('draggable', 'true');
 
             this.el.view = this;
         }
