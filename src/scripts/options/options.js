b26054ff10f3c09d127cd1aed493c75bcc7b0319
zakius <2071503+zakius@users.noreply.github.com>
2021-01-15 12:11:14 +0100

    Fix settings not working on Chromium based browsers

--- src/scripts/options/options.js
+++ src/scripts/options/options.js
@@ -64,11 +64,11 @@ chrome.runtime.getBackgroundPage((bg) => {
             warning.parentNode.removeChild(warning);
         }
         document.querySelector('#version').textContent = chrome.runtime.getManifest().version;
-        browser.runtime.getBrowserInfo().then((info) => {
-            console.log(info);
-            document.querySelector('#browser-info').textContent = `${info.vendor} ${info.name} ${info.version} ${info.buildID}`;
-        });
-
+        if(typeof browser !== 'undefined') {
+            browser.runtime.getBrowserInfo().then((info) => {
+                document.querySelector('#browser-info').textContent = `${info.vendor} ${info.name} ${info.version} ${info.buildID}`;
+            });
+        }
 
         [...document.querySelectorAll('select[id], input[type=number], input[type=range], textarea')].forEach((item) => {
             item.value = bg.settings.get(item.id);
