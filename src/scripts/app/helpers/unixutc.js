155e834a34e4821c4432cc3fafe3d8a836df3bb1
zakius <2071503+zakius@users.noreply.github.com>
2021-04-24 17:22:53 +0200

    Switch to simplified CommonJS wrapper

--- src/scripts/app/helpers/unixutc.js
+++ src/scripts/app/helpers/unixutc.js
@@ -4,9 +4,9 @@
  * @submodule helpers/unixutc
  * @param date {string|Date} Get the week based on this date
  */
-define([], function() {
-	const unixUtcOffset = (new Date).getTimezoneOffset() * 60000;
-	return function(date) {
-		return date.getTime() - unixUtcOffset;
-	};
+define(function () {
+    const unixUtcOffset = (new Date).getTimezoneOffset() * 60000;
+    return function (date) {
+        return date.getTime() - unixUtcOffset;
+    };
 });
