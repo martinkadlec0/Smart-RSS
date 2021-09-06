230cefcb61fd0125aa6bda8d00a9c4b781ecfa3f
zakius <2071503+zakius@users.noreply.github.com>
2021-09-06 22:42:54 +0200

    always display status bar

--- src/scripts/app/views/IndicatorView.js
+++ src/scripts/app/views/IndicatorView.js
@@ -32,17 +32,15 @@ define(['backbone', 'modules/Locale', 'text!templates/indicatorView.html'], func
             let port = chrome.runtime.connect({name: 'port-from-cs'});
             port.onMessage.addListener((m) => {
                 if (m.key === 'loading') {
-                    this.handleLoadingChange();
+                    this.loading = m.value;
                 }
                 if (m.key === 'loaded') {
                     this.loaded = m.value;
-                    this.render();
                 }
                 if (m.key === 'maxSources') {
                     this.maxSources = m.value;
-                    this.render();
                 }
-
+                this.render();
             });
             bg.sources.on('clear-events', this.handleClearEvents, this);
 
@@ -70,34 +68,22 @@ define(['backbone', 'modules/Locale', 'text!templates/indicatorView.html'], func
         },
 
         /**
-         * Hides/shows indicator according to loading flag
-         * @method handleLoadingChange
-         */
-        handleLoadingChange: function () {
-            const value2 = bg.loader.loading;
-            if (value2) {
-                this.render();
-                this.el.classList.add('indicator-visible');
-            } else {
-                setTimeout(() => {
-                    this.el.classList.remove('indicator-visible');
-                }, 500);
-            }
-        },
-
-        /**
          * Renders the indicator (gradient/text)
          * @method render
          * @chainable
          */
         render: function () {
+            this.el.classList.add('indicator-visible');
+
             const {loaded, maxSources} = this;
-            if (maxSources === 0) {
+            if (maxSources === 0 || !this.loading) {
+                this.el.classList.add('indicator-invisible');
                 return;
             }
             const percentage = Math.round(loaded * 100 / maxSources);
             this.el.querySelector('#indicator-progress').style.background = 'linear-gradient(to right,  #c5c5c5 ' + percentage + '%, #eee ' + percentage + '%)';
             this.el.querySelector('#indicator-progress').textContent = Locale.UPDATING_FEEDS + ' (' + loaded + '/' + maxSources + ')';
+            this.el.classList.remove('indicator-invisible');
             return this;
         }
     });
