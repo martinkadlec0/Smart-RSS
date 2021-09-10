433966134297eaed0fb951fa944206e580dff399
zakius <2071503+zakius@users.noreply.github.com>
2021-09-11 00:53:09 +0200

    Rename

--- src/scripts/app/collections/Actions.js
+++ src/scripts/app/collections/Actions.js
@@ -5,7 +5,7 @@
 define(function (require) {
     const BB = require('backbone');
     const Action = require('models/Action');
-    const db = require('staticdb/actions');
+    const actions = require('staticdb/actions');
 
     /**
      * Collection of executable actions. Actions are usually executed by shortcuts, buttons or context menus.
@@ -20,9 +20,9 @@ define(function (require) {
          * @method initialize
          */
         initialize: function () {
-            Object.keys(db).forEach((region) => {
-                Object.keys(db[region]).forEach((name) => {
-                    const c = db[region][name];
+            Object.keys(actions).forEach((region) => {
+                Object.keys(actions[region]).forEach((name) => {
+                    const c = actions[region][name];
                     this.add({name: region + ':' + name, fn: c.fn, icon: c.icon, title: c.title});
                 });
             });
